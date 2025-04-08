290431b33115d4c8b84ce429a62b3c2cff10510d
ProbashDutta <PROBASHDUTTA10@GMAIL.COM>
2025-04-08 23:54:31 +0530

    new page added

---
+++ securixhtml-10/files/user profile.js
@@ -0,0 +1,14 @@
+// You can add interactivity here if needed
+// e.g., Handler for Edit buttons
+
+document.querySelector('.edit-profile').addEventListener('click', function() {
+    alert('Edit Profile Clicked');
+});
+
+document.querySelector('.edit-address').addEventListener('click', function() {
+    alert('Edit Address Clicked');
+});
+
+document.querySelector('.add-user').addEventListener('click', function() {
+    alert('Add User Clicked');
+});
